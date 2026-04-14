e0dc48769e282197c0933ace55d23c6d703334ac
GitGitRice <steven_tanu@hotmail.com>
2026-04-14 11:28:42 +0200

    Implement cart functions

--- cart.py
+++ cart.py
@@ -1,19 +1,44 @@
-from products import print_products
+from products import print_products, select_product
+
 
 # cart als leere liste
+cart = []
+
 
 # reads user input and adds product to cart in loop
-# input product
+# input index, product name, price
 def add_product_to_cart():
-    # code here
-    print_products()
+    while True:
+        print_products()
+        number = input(f"Bitte wähle ein Produkt aus. ")
+        product = select_product(number)
+        cart.append(product)
+        while True:
+            cancel = False
+            answer = input("Möchtest du ein weiteres Produkt hinzufügen? \n'j' für Ja\n'n' für Nein")
+            if answer == "j":
+                break
+            elif answer == "n":
+                cancel = True
+                break
+            else:
+                print("Fehlerhafte Eingabe. Bitte versuche es erneut.")
+                continue
+        if cancel == True:
+            break
+
 
 # print cart
 # no output
 def show_cart():
-    # code here
+    for product in cart:
+        print(product["name"], product["price"])
+        
 
 # calculates cart price sum
 # output summe der preise
 def calculate_cart():
-    # code here
+    sum_cart = 0
+    for product in cart:
+        sum_cart += product["price"]
+    return sum_cart
