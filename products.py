27399b588b1ad5c1e5ef7be5f51a80317506b1cc
Carlos <carlos.brockmann@student.syntax-institut.de>
2026-04-14 11:41:48 +0200

    Print_products

--- products.py
+++ products.py
@@ -1,9 +1,30 @@
 # product: {index, name, price}
+# Liste von Produkten 
+_listeVonProdukten = [
+    {"name" : "Kamera von Marvin", "price": 5000 }, 
+    {"name" : "Kondensatormikrofon", "price": 1000 }, 
+    {"name" : "Mischpult", "price": 1500 }, 
+    {"name" : "Spotstrahler", "price": 200 }, 
+]
+
 
 # print index numbers, product names and prices
 # 1 - Auto
 # 2 - Kaffeemaschine
+
 def print_products():
+    liste = _print_products_def();    
+    for myprod in liste: 
+        print(myprod) 
+
+def _print_products_def():
+    product_list = []
+    i = 0
+    for p in _listeVonProdukten: 
+        product_list.append({"index": i, "name": p["name"], "preis": p["price"]});
+        i = i + 1;
+    return product_list  
+
     # code here
 
 # produkt zu zahl zurückgeben
@@ -11,3 +32,10 @@ def print_products():
 # output -> produkt mit diesem index zurückgeben
 def select_product(number):
     # code here
+    print("bla") 
+
+
+print_products();
+
+
+
